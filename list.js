b7144526757f5f1360431af0422729139030955b
todoom1 <aa13976383@outlook.com>
2025-12-23 13:02:03 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,160 @@
+document.getElementById("yw28").addEventListener("click", function() {
+  window.location.href = "https://xjvueb.hefaship.com/app/register.php?site_id=1017&topId=6393351";
+});
+document.getElementById("ff").addEventListener("click", function() {
+  window.location.href = "https://bakmry.gziasti.com/app/register.php?site_id=1050&topId=624345&selfPlanId=411821";
+});
+document.getElementById("wd").addEventListener("click", function() {
+  window.location.href = "https://onkrcy.hefaship.com/app/register.php?site_id=1020&topId=2926113";
+});
+document.getElementById("df").addEventListener("click", function() {
+  window.location.href = "https://gfvgx.jgqvji.com/app/register.php?site_id=1030&topId=442387";
+});
+document.getElementById("zt").addEventListener("click", function() {
+  window.location.href = "https://pkajim.hfjcl.com/app/register.php?site_id=1031&topId=34033";
+});
+document.getElementById("cf").addEventListener("click", function() {
+  window.location.href = "https://pkajim.hfjcl.com/app/register.php?site_id=1032&topId=897630";
+});
+document.getElementById("c7").addEventListener("click", function() {
+  window.location.href = "https://103.39.18.211:7788/#/link?allwin=7QjUEXXksWwa6da6JQE%2Bsg%3D%3D";
+});
+document.getElementById("ng").addEventListener("click", function() {
+  window.location.href = "https://156.234.1.131:8866/#/link?allwin=11613170";
+});
+document.getElementById("t1").addEventListener("click", function() {
+  window.location.href = "https://bakmry.gziasti.com/app/register.php?site_id=1051&topId=536004";
+});
+document.getElementById("sj").addEventListener("click", function() {
+  window.location.href = "https://xjvueb.hefaship.com/app/register.php?site_id=1010&topId=3113235";
+});
+document.getElementById("28q").addEventListener("click", function() {
+  window.location.href = "https://45.114.105.123:7788/#/link?allwin=8306164";
+});
+document.getElementById("ngty").addEventListener("click", function() {
+  window.location.href = "https://nvjmce.hfjcl.com/app/register.php?site_id=1019&topId=11904188";
+});
+document.getElementById('st').addEventListener('click', function() {
+	alert("胜天国际将预定于12月底上线！");
+});
+
+document.getElementById("0").innerHTML="🔥十年信誉平台推荐";
+
+document.getElementById("1").innerHTML="超凡国际";
+
+document.getElementById("2").innerHTML="非凡娱乐";
+
+document.getElementById("3").innerHTML="问鼎娱乐";
+
+document.getElementById("4").innerHTML="巅峰国际";
+
+document.getElementById("5").innerHTML="NG体育";
+
+document.getElementById("6").innerHTML="征途国际";
+
+document.getElementById("7").innerHTML="28圈";
+
+document.getElementById("8").innerHTML="NG28";
+
+document.getElementById("9").innerHTML="赏金国际";
+
+document.getElementById("10").innerHTML="Top1";
+
+document.getElementById("11").innerHTML="胜天国际";
+
+document.getElementById("58").innerHTML="c7";
+
+document.getElementById("59").innerHTML="亿万28";
+
+document.getElementById("12").innerHTML="PG模拟器";
+
+document.getElementById("13").innerHTML="PP模拟器";
+
+document.getElementById("14").innerHTML="JBD模拟器";
+
+document.getElementById("15").innerHTML="MG模拟器";
+
+document.getElementById("16").innerHTML="AP模拟器";
+
+document.getElementById("17").innerHTML="BBIN模拟器";
+
+document.getElementById("18").innerHTML="PS模拟器";
+
+document.getElementById("19").innerHTML="棋牌模拟器";
+
+document.getElementById("20").innerHTML="cg模拟器";
+
+document.getElementById("21").innerHTML="R88模拟器";
+
+document.getElementById("22").innerHTML="CQ9模拟器";
+
+document.getElementById("23").innerHTML="KA模拟器";
+
+document.getElementById("24").innerHTML="GR模拟器";
+
+document.getElementById("25").innerHTML="百家乐模拟器";
+
+document.getElementById("26").innerHTML="体育模拟器";
+
+document.getElementById("27").innerHTML="彩票模拟器";
+
+document.getElementById("28").innerHTML="爆庄秘籍";
+
+document.getElementById("29").innerHTML="财神捕鱼";
+
+document.getElementById("30").innerHTML="五龙捕鱼";
+
+document.getElementById("31").innerHTML="金爆鱼场";
+
+document.getElementById("32").innerHTML="星际捕鱼";
+
+document.getElementById("33").innerHTML="欢乐捕鱼";
+
+document.getElementById("34").innerHTML="一炮捕鱼";
+
+document.getElementById("35").innerHTML="皇金渔场";
+
+document.getElementById("36").innerHTML="皇金渔场2";
+
+document.getElementById("37").innerHTML="欧易";
+
+document.getElementById("51").innerHTML="币安";
+
+document.getElementById("38").innerHTML="虚拟币入门";
+
+document.getElementById("39").innerHTML="狗狗云vpn";
+
+document.getElementById("40").innerHTML="杜卡迪vpn";
+
+document.getElementById("41").innerHTML="大麦VPN";
+
+document.getElementById("42").innerHTML="今日要闻";
+
+document.getElementById("43").innerHTML="海外ID";
+
+document.getElementById("44").innerHTML="软件大全";
+
+document.getElementById("45").innerHTML="短信接码";
+
+document.getElementById("46").innerHTML="jable";
+
+document.getElementById("47").innerHTML="youporn";
+
+document.getElementById("48").innerHTML="njav";
+
+document.getElementById("49").innerHTML="pornhub";
+
+document.getElementById("50").innerHTML="Airav";
+
+document.getElementById("52").innerHTML="🔥PG模拟器大全";
+
+document.getElementById("53").innerHTML="捕鱼模拟器";
+
+document.getElementById("54").innerHTML="虚拟币";
+
+document.getElementById("55").innerHTML="其他分享&VPN-开VPN可进蓝名";
+
+document.getElementById("56").innerHTML="如遇本站功能无法正常使用，请点击下载注册狗狗云VPN，即可突破网络封锁正常使用";
+
+document.getElementById("57").innerHTML="zx12.my";
+
