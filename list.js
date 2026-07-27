b6683eb35393f7a3e14bd4aab4f898d5e6078494
todoom1 <aa13976383@outlook.com>
2026-07-28 00:29:04 +0800

    Refactor secretApps array and update DOM manipulation

--- list.js
+++ list.js
@@ -1,5 +1,6 @@
 (function() {
     var secretApps = [
+{img: './png/zhgj.jpg', name: '纵横国际', xurl: 'aHR0cDovL3hucXd6Zi50Y2txeHguY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDY3JnRvcElkPTk4MzE3ODUxNjQxMzg1ODAmc2VsZlBsYW5JZD04MDc='},
 {img: './png/ltgj.jpg', name: '龙腾国际', xurl: 'aHR0cHM6Ly9wc2t0a3gudGNrcXh4LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA2NSZ0b3BJZD0yNjgyOSZzZWxmUGxhbklkPTEzMTM='},
 {img: './png/xjh.jpg', name: '聚鑫汇', xurl: 'aHR0cHM6Ly95Ymt2YnoudGNrcXh4LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA2NiZ0b3BJZD0yNzA4NSZzZWxmUGxhbklkPTE1MDk='},
 {img: './png/lhgj.jpg', name: '领航国际', xurl: 'aHR0cHM6Ly9yc2VzcmEuaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDY0JnB0PTM3NTI2QkZBLTNDOEEtRjNDNC1ERDU3LUE3MjI2NzNCNzcxQw=='},
