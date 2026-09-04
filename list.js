203c3d411b8d3effcc74e850170d8f8009580f9c
todoom1 <aa13976383@outlook.com>
2026-09-04 13:55:04 +0800

    Refactor secretApps initialization and event handling

--- list.js
+++ list.js
@@ -10,7 +10,7 @@
 {img: './png/ffyl.jpg', name: '非凡国际', xurl: 'aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTI0NjE3NjM0NjgxNDE4MzUmc2VsZlBsYW5JZD00MTE4MjE='},
 {img: './png/ztgj.jpg', name: '征途国际', xurl: 'aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMxJnRvcElkPTM0MDMz'},
 {img: './png/dfgj.jpg', name: '巅峰国际', xurl: 'aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMwJnRvcElkPTQ0MjM4Nw=='},
-{img: './svg/28q.svg', name: '28圈', xurl: 'aHR0cHM6Ly80NS4xMTQuMTA1LjEyMzo3Nzg4LyMvbGluaz9hbGx3aW49ODMwNjE2NA=='},
+{img: './svg/28q.svg', name: '28圈', xurl: 'aHR0cHM6Ly80My4yNDkuMTc1LjEwOjYxMTA4LyMvbGluaz9hbGx3aW49eVUwMFB0ODMwNjE2NA=='},
 {img: './svg/wd.svg', name: '问鼎娱乐', xurl: 'aHR0cHM6Ly9vbmtyY3kuaGVmYXNoaXAuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDIwJnRvcElkPTI5MjYxMTM='},
 {img: './png/sjgj.png', name: '赏金国际', xurl: 'aHR0cHM6Ly94anZ1ZWIuaGVmYXNoaXAuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDEwJnRvcElkPTMxMTMyMzU='},
 {img: './png/c7.png', name: 'C7', xurl: 'aHR0cHM6Ly8zOC4xODEuMjMuMTAyOjYwMDA1LyMvbGluaz9hbGx3aW49N1FqVUVYWGtzV3dhNmRhNkpRRSUyQnNnJTNEJTNE'},
